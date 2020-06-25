2e68b015cd68d88e03ec18c66b9eb3921fd6e4dd
Rafael Franco <me@rafaelfranco.es>
2020-06-25 21:22:33 +0100

    Added materials

---
+++ pkg/tracer/material.go
@@ -0,0 +1,45 @@
+package tracer
+
+import "math"
+
+type Material struct {
+	Color                                 *Tup
+	Ambient, Diffuse, Specular, Shininess float64
+}
+
+func NewMaterial() *Material {
+	return &Material{
+		Color:     Color(1, 1, 1),
+		Ambient:   0.1,
+		Diffuse:   0.9,
+		Specular:  0.9,
+		Shininess: 200,
+	}
+}
+
+func (m *Material) Lighting(l *PointLight, p *Tup, eyev *Tup, normv *Tup) *Tup {
+	var ambient, diffuse, specular *Tup
+
+	effectiveColor := MulTup(m.Color, l.Intensity)
+	lightv := SubTup(l.Position, p).Normalize()
+	ambient = ScaleTup(effectiveColor, m.Ambient)
+	lightDotNormal := DotTup(lightv, normv)
+
+	if lightDotNormal < 0 {
+		diffuse = Color(0, 0, 0)
+		specular = Color(0, 0, 0)
+	} else {
+		diffuse = ScaleTup(effectiveColor, m.Diffuse*lightDotNormal)
+		reflectv := NegTup(lightv).Reflect(normv)
+		reflectDotEye := DotTup(reflectv, eyev)
+
+		if reflectDotEye <= 0 {
+			specular = Color(0, 0, 0)
+		} else {
+			factor := math.Pow(reflectDotEye, m.Shininess)
+			specular = ScaleTup(l.Intensity, m.Specular*factor)
+		}
+	}
+
+	return AddTup(AddTup(ambient, diffuse), specular)
+}
---
+++ pkg/tracer/material_test.go
@@ -0,0 +1,92 @@
+package tracer
+
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMaterial(t *testing.T) {
+	t.Parallel()
+
+	t.Run("the default material", func(t *testing.T) {
+		t.Parallel()
+
+		m := NewMaterial()
+
+		assert.Equal(t, Color(1, 1, 1), m.Color)
+		assert.Equal(t, .1, m.Ambient)
+		assert.Equal(t, .9, m.Diffuse)
+		assert.Equal(t, .9, m.Specular)
+		assert.Equal(t, 200.0, m.Shininess)
+	})
+
+	mat := NewMaterial()
+	pos := Point(0, 0, 0)
+	cmp := &Cmp{10e-5}
+
+	t.Run("lighting with the eye between the light and the surface", func(t *testing.T) {
+		t.Parallel()
+
+		eyev := Vector(0, 0, -1)
+		normalv := Vector(0, 0, -1)
+		light := &PointLight{Point(0, 0, -10), Color(1, 1, 1)}
+		result := mat.Lighting(light, pos, eyev, normalv)
+
+		assert.Equal(t, Color(1.9, 1.9, 1.9), result)
+	})
+
+	t.Run("lighting with the eye between the light and the surface", func(t *testing.T) {
+		t.Parallel()
+
+		p := math.Sqrt(2) / 2.0
+		eyev := Vector(0, p, p)
+		normalv := Vector(0, 0, -1)
+		light := &PointLight{Point(0, 0, -10), Color(1, 1, 1)}
+		result := mat.Lighting(light, pos, eyev, normalv)
+
+		assert.Equal(t, Color(1.0, 1.0, 1.0), result)
+	})
+
+	t.Run("lighting with the eye opposite surface, light at 45deg", func(t *testing.T) {
+		t.Parallel()
+
+		p := math.Sqrt(2) / 2.0
+
+		eyev := Vector(0, 0, -1)
+		normalv := Vector(0, 0, -1)
+		light := &PointLight{Point(0, 10, -10), Color(1, 1, 1)}
+		result := mat.Lighting(light, pos, eyev, normalv)
+
+		r := .1 + p*.9
+		assert.True(t, cmp.Equal(r, result.Red()))
+		assert.True(t, cmp.Equal(r, result.Green()))
+		assert.True(t, cmp.Equal(r, result.Blue()))
+	})
+
+	t.Run("lighting with the eye in the path of the reflection vector", func(t *testing.T) {
+		t.Parallel()
+
+		p := math.Sqrt(2) / 2.0
+
+		eyev := Vector(0, -p, -p)
+		normalv := Vector(0, 0, -1)
+		light := &PointLight{Point(0, 10, -10), Color(1, 1, 1)}
+		result := mat.Lighting(light, pos, eyev, normalv)
+
+		r := .1 + .9*p + .9
+		assert.Equal(t, Color(r, r, r), result)
+	})
+
+	t.Run("lighting with the light behind the surface", func(t *testing.T) {
+		t.Parallel()
+
+		eyev := Vector(0, 0, -1)
+		normalv := Vector(0, 0, -1)
+		light := &PointLight{Point(0, 0, 10), Color(1, 1, 1)}
+		result := mat.Lighting(light, pos, eyev, normalv)
+
+		assert.Equal(t, Color(.1, .1, .1), result)
+	})
+}
