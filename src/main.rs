1f95d4ff54e44680ba19448e1b22bdbe04bbe722
Rafael Franco <me@rafaelfranco.es>
2020-07-04 16:03:46 +0100

    create scene on main, use opengl canvas

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,41 @@
-mod tuple;
+mod camera;
+mod canvas;
+mod intersections;
+mod light;
+mod material;
 mod matrix;
+mod objects;
+mod ray;
+mod transformations;
+mod tuple;
+mod world;
+
+const HEIGHT: u32 = 1000;
+const WIDTH: u32 = 1000;
 
 fn main() {
-    let p = tuple::point(1.0, 3.0, 4.0);
-    let v = tuple::vector(0.9, 0.3, 0.1);
+    let mut world = world::World::new();
+    world
+        .objects
+        .push(objects::Object::Sphere(objects::Sphere::new()));
+    world.light = light::PointLight {
+        intensity: tuple::color(1.0, 1.0, 1.0),
+        position: tuple::point(-10.0, 10.0, -10.0),
+    };
 
-    println!("{} {}", p, v);
+    let mut camera = camera::Camera::new(HEIGHT as f64, WIDTH as f64, std::f64::consts::PI / 2.0);
+    camera.transform = transformations::view(
+        tuple::point(0.0, 1.5, -5.0),
+        tuple::point(0.0, 1.0, 0.0),
+        tuple::vector(0.0, 1.0, 0.0),
+    );
 
-    println!("{}", &p + &v);
-    println!("{}", &p - &v);
-    println!("{}", p == v);
-    println!("{}", p == v);
-    println!("{}", -&p)
+    let mut canvas = canvas::OpenGLCanvas::new(HEIGHT, WIDTH, "OpenGL Canvas".to_string());
+
+    // this should somehow run concurrently
+    camera.render(world, &mut canvas);
+    
+    // with this. this should also run in the main thread because opengl needs some thread-local
+    // state.
+    canvas.run();
 }
