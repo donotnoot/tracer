63f9a5c49c3c87712fc93da232a9b04d7404efee
Rafael Franco <me@rafaelfranco.es>
2020-07-06 17:17:50 +0100

    material: add tests, fix bugs

--- src/material.rs
+++ src/material.rs
@@ -1,7 +1,7 @@
 use super::light::PointLight;
-use super::tuple::{color, dot, Tup};
+use super::tuple::{color, dot, point, vector, Tup};
 
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 pub struct Material {
     pub color: Tup,
     pub ambient: f64,
@@ -21,11 +21,11 @@ impl Material {
         }
     }
 
-    pub fn lighting(&self, l: &PointLight, p: Tup, eye: Tup, normal: Tup, inShadow: bool) -> Tup {
+    pub fn lighting(&self, l: &PointLight, p: Tup, eye: Tup, normal: Tup, in_shadow: bool) -> Tup {
         let effective_color = &self.color * &l.intensity;
         let ambient = &effective_color * self.ambient;
 
-        if inShadow {
+        if in_shadow {
             return ambient;
         }
 
@@ -37,7 +37,7 @@ impl Material {
             let specular = color(0.0, 0.0, 0.0);
             return &ambient + &(&diffuse + &specular);
         } else {
-            let diffuse = &effective_color * (&self.diffuse * &light_normal_dot);
+            let diffuse = &effective_color * (self.diffuse * light_normal_dot);
             let reflect = &(-&light).reflect(&normal);
             let reflect_dot_eye = dot(&reflect, &eye);
 
@@ -46,7 +46,7 @@ impl Material {
                 return &ambient + &(&diffuse + &specular);
             } else {
                 let factor = reflect_dot_eye.powf(self.shininess);
-                let specular = &l.intensity * (&self.specular * factor);
+                let specular = &l.intensity * (self.specular * factor);
                 return &ambient + &(&diffuse + &specular);
             }
         }
@@ -55,4 +55,123 @@ impl Material {
 
 pub trait HasMaterial {
     fn material(&self) -> Material;
+}
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn eye_between_light_and_surface() {
+        let mat = Material::new();
+        let pos = point(0.0, 0.0, 0.0);
+
+        let eyev = vector(0.0, 0.0, -1.0);
+        let normalv = vector(0.0, 0.0, -1.0);
+        let light = PointLight {
+            position: point(0.0, 0.0, -10.0),
+            intensity: color(1.0, 1.0, 1.0),
+        };
+        let result = mat.lighting(&light, pos, eyev, normalv, false);
+
+        assert_eq!((1.9 - result.x).abs() <= std::f64::EPSILON, true);
+        assert_eq!((1.9 - result.y).abs() <= std::f64::EPSILON, true);
+        assert_eq!((1.9 - result.z).abs() <= std::f64::EPSILON, true);
+    }
+
+    #[test]
+    fn eye_between_light_and_surface_45deg_off() {
+        let mat = Material::new();
+        let pos = point(0.0, 0.0, 0.0);
+
+        let p = 2_f64.sqrt() / 2.0;
+        let eyev = vector(0.0, p, p);
+        let normalv = vector(0.0, 0.0, -1.0);
+        let light = PointLight {
+            position: point(0.0, 0.0, -10.0),
+            intensity: color(1.0, 1.0, 1.0),
+        };
+        let result = mat.lighting(&light, pos, eyev, normalv, false);
+
+        assert_eq!((1.0 - result.x).abs() <= std::f64::EPSILON, true);
+        assert_eq!((1.0 - result.y).abs() <= std::f64::EPSILON, true);
+        assert_eq!((1.0 - result.z).abs() <= std::f64::EPSILON, true);
+    }
+
+    #[test]
+    fn eye_opposite_surface_light_45_deg() {
+        let mat = Material::new();
+        let pos = point(0.0, 0.0, 0.0);
+
+        let p = 2_f64.sqrt() / 2.0;
+
+        let eyev = vector(0.0, 0.0, -1.0);
+        let normalv = vector(0.0, 0.0, -1.0);
+        let light = PointLight {
+            position: point(0.0, 10.0, -10.0),
+            intensity: color(1.0, 1.0, 1.0),
+        };
+        let result = mat.lighting(&light, pos, eyev, normalv, false);
+
+        let r = 0.1 + p * 0.9;
+        assert_eq!((r - result.x).abs() <= std::f64::EPSILON, true);
+        assert_eq!((r - result.y).abs() <= std::f64::EPSILON, true);
+        assert_eq!((r - result.z).abs() <= std::f64::EPSILON, true);
+    }
+
+    #[test]
+    fn eye_in_the_path_of_reflection_vector() {
+        let mat = Material::new();
+        let pos = point(0.0, 0.0, 0.0);
+
+        let p = 2_f64.sqrt() / 2.0;
+
+        let eyev = vector(0.0, -p, -p);
+        let normalv = vector(0.0, 0.0, -1.0);
+        let light = PointLight {
+            position: point(0.0, 10.0, -10.0),
+            intensity: color(1.0, 1.0, 1.0),
+        };
+        let result = mat.lighting(&light, pos, eyev, normalv, false);
+
+        let r = 0.1 + 0.9 * p + 0.9;
+        assert_eq!((r - result.x).abs() <= std::f64::EPSILON, true);
+        assert_eq!((r - result.y).abs() <= std::f64::EPSILON, true);
+        assert_eq!((r - result.z).abs() <= std::f64::EPSILON, true);
+    }
+
+    #[test]
+    fn light_behind_surface() {
+        let mat = Material::new();
+        let pos = point(0.0, 0.0, 0.0);
+
+        let eyev = vector(0.0, 0.0, -1.0);
+        let normalv = vector(0.0, 0.0, -1.0);
+        let light = PointLight {
+            position: point(0.0, 0.0, 10.0),
+            intensity: color(1.0, 1.0, 1.0),
+        };
+        let result = mat.lighting(&light, pos, eyev, normalv, false);
+
+        assert_eq!((0.1 - result.x).abs() <= std::f64::EPSILON, true);
+        assert_eq!((0.1 - result.y).abs() <= std::f64::EPSILON, true);
+        assert_eq!((0.1 - result.z).abs() <= std::f64::EPSILON, true);
+    }
+
+    #[test]
+    fn surface_in_shadow() {
+        let mat = Material::new();
+        let pos = point(0.0, 0.0, 0.0);
+
+        let eyev = vector(0.0, 0.0, -1.0);
+        let normalv = vector(0.0, 0.0, -1.0);
+        let light = PointLight {
+            position: point(0.0, 0.0, -10.0),
+            intensity: color(1.0, 1.0, 1.0),
+        };
+        let result = mat.lighting(&light, pos, eyev, normalv, true);
+
+        assert_eq!((0.1 - result.x).abs() <= std::f64::EPSILON, true);
+        assert_eq!((0.1 - result.y).abs() <= std::f64::EPSILON, true);
+        assert_eq!((0.1 - result.z).abs() <= std::f64::EPSILON, true);
+    }
 }
