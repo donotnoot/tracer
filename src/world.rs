7228e214326a261e2e8e651a348389cb8ce91be7
Rafael Franco <me@rafaelfranco.es>
2020-07-06 17:19:02 +0100

    world: added default and test

--- src/world.rs
+++ src/world.rs
@@ -1,8 +1,9 @@
 use super::intersections::{hit, Computations, Intersect, Intersections};
 use super::light::PointLight;
 use super::material::HasMaterial;
-use super::objects::Object;
+use super::objects::{Object, Sphere};
 use super::ray::Ray;
+use super::transformations::scaling;
 use super::tuple::{color, point, vector, Tup};
 
 pub struct World {
@@ -13,7 +14,20 @@ pub struct World {
 impl World {
     pub fn new() -> Self {
         World {
-            objects: vec![],
+            objects: vec![
+                {
+                    let mut s = Sphere::new();
+                    s.material.color = color(0.8, 1.0, 0.6);
+                    s.material.diffuse = 0.7;
+                    s.material.specular = 0.2;
+                    Object::Sphere(s)
+                },
+                {
+                    let mut s = Sphere::new();
+                    s.transform = scaling(0.5, 0.5, 0.5);
+                    Object::Sphere(s)
+                },
+            ],
             light: PointLight {
                 position: point(-10.0, 10.0, -10.0),
                 intensity: vector(-10.0, 10.0, -10.0),
@@ -27,6 +41,8 @@ impl World {
         for (_, elem) in self.objects.iter().enumerate() {
             i.append(&mut (*elem).intersect(r));
         }
+
+        i.sort_by(|a, b| a.t.partial_cmp(&b.t).unwrap());
 
         i
     }
@@ -61,5 +77,28 @@ impl World {
         c.object
             .material()
             .lighting(&self.light, c.point, c.eye, c.normal, s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn intersecting_world_with_ray() {
+        let w = World::new();
+        let r = Ray {
+            origin: point(0.0, 0.0, -5.0),
+            direction: vector(0.0, 0.0, 1.0),
+        };
+        let ixs = w.intersect(&r);
+
+        ixs.iter().for_each(|e| println!("{:?}", e));
+
+        assert_eq!(ixs.len(), 4);
+        assert_eq!(ixs[0].t, 4.0);
+        assert_eq!(ixs[1].t, 4.5);
+        assert_eq!(ixs[2].t, 5.5);
+        assert_eq!(ixs[3].t, 6.0);
     }
 }
