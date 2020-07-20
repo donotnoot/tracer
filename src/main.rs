eb6e8f5c0afee8aa7d9ccd559e420d92a8100f2f
Rafael Franco <me@rafaelfranco.es>
2020-07-21 00:39:31 +0100

    main: updated example

--- src/main.rs
+++ src/main.rs
@@ -24,29 +24,29 @@ const WIDTH: u32 = 1920;
 
 fn main() {
     // color palette
-    let P_DARK: Tup = color_u8(0x0A, 0x11, 0x28);
-    let P_MED_DARK: Tup = color_u8(0x00, 0x1F, 0x54);
-    let P_MED: Tup = color_u8(0x3, 0x40, 0x78);
-    let P_MED_LIGHT: Tup = color_u8(0x12, 0x82, 0xA2);
-    let P_LIGHT: Tup = color_u8(0xFE, 0xFC, 0xFB);
+    let P_DARK: Tup = color_u8(0x10, 0x10, 0x10);
+    let P_MED_DARK: Tup = color_u8(0x30, 0x30, 0x30);
+    let P_MED: Tup = color_u8(0x43, 0x92, 0xF1);
+    let P_MED_LIGHT: Tup = color_u8(0xDC, 0x49, 0x3A);
+    let P_LIGHT: Tup = color_u8(0xE3, 0xEB, 0xFF);
 
     let mut world = world::World::new();
     world.objects = vec![];
-    world.background_color = color(0.8, 0.8, 0.85);
+    world.background_color = color(1.0, 1.0, 1.0);
 
     {
         let mut floor = objects::Plane::new();
-        floor.material.color = color(1.0, 0.9, 0.9);
+        floor.material.color = color(1.0, 1.0, 1.0);
         floor.material.specular = 0.0;
-        floor.material.reflectiveness = 0.3;
-        floor.material.pattern = Some(Pattern::Checker(P_DARK.clone(), P_MED_DARK.clone(), None));
+        floor.material.reflectiveness = 0.1;
+        floor.material.pattern = Some(Pattern::Ring(P_DARK.clone(), P_MED_DARK.clone(), None));
         world.objects.push(objects::Object::Plane(floor));
     }
 
     {
         let mut backdrop = objects::Plane::new();
         backdrop.transform = translation(0.0, 0.0, 3.0) * rotate_x(std::f32::consts::PI / 2.0);
-        backdrop.material.color = color(1.0, 0.9, 0.9);
+        backdrop.material.color = color(1.0, 1.0, 1.0);
         backdrop.material.specular = 0.0;
         backdrop.material.pattern =
             Some(Pattern::Checker(P_LIGHT.clone(), P_MED_LIGHT.clone(), None));
@@ -54,24 +54,13 @@ fn main() {
     }
 
     {
-        let mut frontdrop = objects::Plane::new();
-        frontdrop.transform =
-            translation(0.0, 0.0, -10.0) * rotate_x(3.0 * std::f32::consts::PI / 2.0);
-        frontdrop.material.color = color(1.0, 1.0, 1.0);
-        frontdrop.material.specular = 0.0;
-        frontdrop.material.pattern =
-            Some(Pattern::Checker(P_MED_LIGHT.clone(), P_LIGHT.clone(), None));
-        // world.objects.push(objects::Object::Plane(frontdrop));
-    }
-
-    {
         let mut object = objects::Sphere::new();
-        object.transform = translation(-2.5, 1.0, 0.0) * scaling(1.5, 1.5, 1.5);
-        object.material.reflectiveness = 0.8;
+        object.transform = translation(-3.5, 1.0, 0.0);// * scaling(2.0,2.0,2.0);
+        object.material.reflectiveness = 0.05;
         object.material.pattern = Some(Pattern::Stripe(
             P_MED.clone(),
             P_MED_LIGHT.clone(),
-            Some(scaling(0.1, 0.1, 0.1)),
+            Some(translation(1.25, 1.0, 1.0) * rotate_z(3.14/1.2) * scaling(0.3, 1.0, 1.0)),
         ));
         world.objects.push(objects::Object::Sphere(object));
     }
@@ -80,11 +69,11 @@ fn main() {
         let mut object = objects::Sphere::new();
         object.transform = translation(0.0, 1.0, 0.0);
         object.material.color = color(1.0, 1.0, 1.0);
-        object.material.reflectiveness = 0.99;
+        object.material.reflectiveness = 0.85;
         object.material.specular = 0.99;
-        object.material.ambient = 0.01;
-        object.material.diffuse = 0.1;
-        object.material.shininess = 500.0;
+        object.material.ambient = 0.0;
+        object.material.diffuse = 0.0;
+        object.material.shininess = 200.0;
         world.objects.push(objects::Object::Sphere(object));
     }
 
@@ -94,7 +83,7 @@ fn main() {
         object.material.pattern = Some(Pattern::Checker(
             P_MED.clone(),
             P_MED_DARK.clone(),
-            Some(scaling(0.1, 0.1, 0.1)),
+            Some(rotate_x(std::f32::consts::PI / 1.3)),
         ));
         world.objects.push(objects::Object::Sphere(object));
     }
@@ -121,7 +110,7 @@ fn main() {
     let (tx, rx): (Sender<canvas::Pixel>, Receiver<canvas::Pixel>) = mpsc::channel();
 
     thread::spawn(move || {
-        camera.render(world, tx, true, 4096);
+        camera.render(world, tx, false, 32);
     });
 
     canvas.run(rx);
