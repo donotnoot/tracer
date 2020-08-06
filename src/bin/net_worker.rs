f6ea61a895c678906d8239da340762912d5ba2a5
Rafael Franco <me@rafaelfranco.es>
2020-08-06 22:17:05 +0100

    net_worker: implemented new network protocol

--- src/bin/net_worker.rs
+++ src/bin/net_worker.rs
@@ -1,148 +1,100 @@
+#![recursion_limit = "1024"]
+
+use futures::{Stream, StreamExt};
+use log::*;
+use pretty_env_logger;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 use rayon::prelude::*;
 use rstracer::tracer::*;
 use std::collections::HashMap;
 use std::error::Error;
+use std::pin::Pin;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use tokio::sync::mpsc;
 use tonic::transport::Server;
-use tonic::{Code, Request, Response, Status};
+use tonic::{Code, Request, Response, Status, Streaming};
 
 pub mod net_render {
     tonic::include_proto!("net_render");
 }
 
+use net_render::job::Request as JobRequest;
 use net_render::worker_server::{Worker, WorkerServer};
-use net_render::{Job, JobResult, Pixel, Spec};
+use net_render::{Job, Pixel, Pixels, Tile};
 
 #[derive(Debug)]
-pub struct WorkerService {
-    pub scenes: Arc<Mutex<HashMap<String, String>>>,
-}
+pub struct WorkerService {}
 
 #[tonic::async_trait]
 impl Worker for WorkerService {
-    async fn scene(&self, request: Request<Spec>) -> Result<Response<()>, Status> {
-        let request = request.into_inner();
+    type RenderStream = Pin<Box<dyn Stream<Item = Result<Pixels, Status>> + Send + Sync + 'static>>;
 
-        let mut lock = self.scenes.lock().unwrap();
-        lock.insert(request.id, request.spec);
+    async fn render(
+        &self,
+        request: Request<Streaming<Job>>,
+    ) -> Result<Response<Self::RenderStream>, Status> {
+        let remote_address = request.remote_addr().unwrap();
+        info!("Accepted request from {}", remote_address);
 
-        Ok(Response::new(()))
-    }
+        let mut stream = request.into_inner();
 
-    type WorkStream = mpsc::Receiver<Result<JobResult, Status>>;
-    async fn work(&self, request: Request<Job>) -> Result<Response<Self::WorkStream>, Status> {
-        println!("new request from {}", request.remote_addr().unwrap());
+        let job = stream.next().await.unwrap().unwrap();
+        let scene = match job.request.unwrap() {
+            JobRequest::Scene(scene) => Ok(scene),
+            _ => Err(Status::new(
+                Code::InvalidArgument,
+                "fist message must be scene!".to_string(),
+            )),
+        }?;
+        info!("Received scene successfully");
 
-        let request = request.into_inner();
+        let (world, camera, rendering_spec) = match scene_parser::from_reader(scene.as_bytes()) {
+            Ok((world, camera, rendering_spec)) => Ok((world, camera, rendering_spec)),
+            Err(err) => Err(Status::new(
+                Code::InvalidArgument,
+                format!("invalid rendering spec: {}", err),
+            )),
+        }?;
+        info!("Parsed scene successfully");
 
-        let (mut tx, rx) = mpsc::channel(32);
-        let (u_tx, mut u_rx) = mpsc::unbounded_channel::<Pixel>();
+        let output = async_stream::try_stream! {
+            while let Some(job) = stream.next().await {
+                let tile = match job?.request.unwrap() {
+                    JobRequest::Tile(tile) => Ok(tile),
+                    _ => Err(Status::new(
+                        Code::InvalidArgument,
+                        "consecutive messages must be tiles!".to_string(),
+                    )),
+                }?;
+                info!("Rendering tile: {:?}", tile);
 
-        let lock = self.scenes.lock().unwrap();
-        let spec = match lock.get(&request.scene_id) {
-            Some(spec_id) => spec_id,
-            None => {
-                return Err(Status::new(
-                    Code::InvalidArgument,
-                    "invalid rendering spec".to_string(),
-                ))
+                let mut pixels: Vec<(u32, u32)> = vec![];
+                for x in tile.x..(tile.x + tile.size) {
+                    for y in tile.y..(tile.y + tile.size) {
+                        pixels.push((x, y));
+                    }
+                }
+
+                let pixels = pixels
+                    .into_par_iter()
+                    .with_max_len(1)
+                    .map(|(x, y)| {
+                        Pixel {
+                            x,
+                            y,
+                            color: tup_to_u32_color(camera.render_pixel(&world, x, y)),
+                        }
+                    }).collect::<Vec<Pixel>>();
+
+                info!("Rendered tile successfully");
+
+                yield Pixels{pixels}
             }
         };
 
-        let (world, camera, rendering_spec) = match scene_parser::from_reader(spec.as_bytes()) {
-            Ok((world, camera, rendering_spec)) => (world, camera, rendering_spec),
-            Err(err) => {
-                return Err(Status::new(
-                    Code::InvalidArgument,
-                    format!("invalid rendering spec: {}", err),
-                ))
-            }
-        };
-
-        println!("{} tile(s)", request.tiles.len());
-        let mut pixels: Vec<(u32, u32, mpsc::UnboundedSender<Pixel>)> = vec![];
-        for tile in request.tiles {
-            println!("x: {}, y: {}, size: {}", tile.x, tile.y, tile.size);
-
-            for x in tile.x..(tile.x + tile.size) {
-                for y in tile.y..(tile.y + tile.size) {
-                    pixels.push((x, y, u_tx.clone()));
-                }
-            }
-        }
-
-        if rendering_spec.randomize_rays {
-            println!("shuffling pixels");
-            pixels.shuffle(&mut thread_rng());
-        }
-
-        println!("processing {} pixels", pixels.len());
-        let worker = thread::spawn(move || -> Result<(), Status> {
-            pixels
-                .into_par_iter()
-                .with_max_len(1)
-                .map(|(x, y, tx)| {
-                    let pixel = Pixel {
-                        x,
-                        y,
-                        color: tup_to_u32_color(camera.render_pixel(&world, x, y)),
-                    };
-                    match tx.send(pixel) {
-                        Ok(()) => Ok(()),
-                        Err(err) => Err(Status::new(
-                            Code::Internal,
-                            format!("could not send pixel between threads: {}", err),
-                        )),
-                    }
-                })
-                .collect::<Result<(), Status>>()
-        });
-
-        tokio::spawn(async move {
-            let mut batch = vec![];
-
-            while let Some(msg) = u_rx.recv().await {
-                batch.push(msg);
-
-                if batch.len() > 100 {
-                    let job = Ok(JobResult {
-                        pixels: batch.clone(),
-                    });
-
-                    if let Err(err) = tx.send(job).await {
-                        return Err(Status::new(
-                            Code::Internal,
-                            format!("could not send pixel batch to client: {}", err),
-                        ));
-                    }
-                    batch = vec![];
-                }
-            }
-
-            let job = Ok(JobResult {
-                pixels: batch.clone(),
-            });
-
-            if let Err(err) = tx.send(job).await {
-                return Err(Status::new(
-                    Code::Internal,
-                    format!("could not send pixel batch to client: {}", err),
-                ));
-            }
-
-            println!("done!");
-
-            match worker.join().unwrap() {
-                Ok(()) => Ok(()),
-                Err(err) => Err(Status::new(Code::Internal, format!("{}", err))),
-            }
-        });
-
-        Ok(Response::new(rx))
+        Ok(Response::new(Box::pin(output) as Self::RenderStream))
     }
 }
 
@@ -153,11 +105,13 @@ fn tup_to_u32_color(t: tuple::Tup) -> u32 {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    pretty_env_logger::formatted_builder()
+        .filter(None, log::LevelFilter::Info)
+        .init();
+
     let addr = "0.0.0.0:9000".parse().unwrap();
-    let svc = WorkerServer::new(WorkerService {
-        scenes: Arc::new(Mutex::new(HashMap::new())),
-    });
-    println!("serving on {}", addr);
+    let svc = WorkerServer::new(WorkerService {});
+    info!("Serving on {}", addr);
     Server::builder().add_service(svc).serve(addr).await?;
     Ok(())
 }
