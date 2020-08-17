ba8a5b38b96f43473ce576fa91b136fac84b0b22
Rafael Franco <me@rafaelfranco.es>
2020-08-17 12:57:27 +0100

    generated proto: updated grpc version

--- netcode/pkg/pb/net_render.pb.go
+++ netcode/pkg/pb/net_render.pb.go
@@ -293,11 +293,11 @@ var fileDescriptor_9ea114fcbf489839 = []byte{
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
-var _ grpc.ClientConnInterface
+var _ grpc.ClientConn
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion6
+const _ = grpc.SupportPackageIsVersion4
 
 // WorkerClient is the client API for Worker service.
 //
@@ -307,10 +307,10 @@ type WorkerClient interface {
 }
 
 type workerClient struct {
-	cc grpc.ClientConnInterface
+	cc *grpc.ClientConn
 }
 
-func NewWorkerClient(cc grpc.ClientConnInterface) WorkerClient {
+func NewWorkerClient(cc *grpc.ClientConn) WorkerClient {
 	return &workerClient{cc}
 }
 
